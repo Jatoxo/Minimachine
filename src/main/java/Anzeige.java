a58a44a0520e09e980f0e4090bac3c0559f7f238
Jojojihn <59710154+Jojojihn@users.noreply.github.com>
2021-09-20 18:55:31 +0200

    Update Anzeige.java

--- src/main/java/Anzeige.java
+++ src/main/java/Anzeige.java
@@ -1,8 +1,3 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by Fernflower decompiler)
-//
-
 /*
 import com.apple.eawt.AboutHandler;
 import com.apple.eawt.Application;
